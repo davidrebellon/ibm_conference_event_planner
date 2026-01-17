bfac3a27c332e333e1cc9f499199e160a49424f6
davidrebellon <david1rebellon@gmail.com>
2026-01-17 07:40:56 -0500

    limit quantity of non auditorium  rooms

--- src/venueSlice.js
+++ src/venueSlice.js
@@ -34,15 +34,18 @@ export const venueSlice = createSlice({
       cost: 1100,
       quantity: 0,
     },
-  
+
   ],
   reducers: {
-   
+
     incrementQuantity: (state, action) => {
       const { payload: index } = action;
       if (state[index]) {
         if (state[index].name === " Auditorium Hall (Capacity:200)" && state[index].quantity >= 3) {
-          return;        }
+          return;
+        } else if (state[index].quantity >= 10) { // limit number of other rooms
+          return;
+        }
         state[index].quantity++;
       }
     },
